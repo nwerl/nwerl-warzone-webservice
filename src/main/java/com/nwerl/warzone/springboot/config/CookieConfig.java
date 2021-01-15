1399f7b89a256df98c2322d48e4fa5e396ffec36
nwerl <msh6242006@gmail.com>
2021-01-15 21:00:02 +0900

    Refactor HttpHeadersConfig
    getCookie method returned Map<String, String> but It was registered as Bean.
    so getCookie and authToken were moved to HttpHeadersConfig.

--- src/main/java/com/nwerl/warzone/springboot/config/CookieConfig.java
+++ src/main/java/com/nwerl/warzone/springboot/config/CookieConfig.java
@@ -1,36 +1,8 @@
 package com.nwerl.warzone.springboot.config;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
-import java.util.Map;
 
 @Configuration
 public class CookieConfig {
-    @Value("${activision.username}")
-    String username;
-    @Value("${activision.password}")
-    String password;
 
-    private Connection.Response authToken() throws IOException {
-        Map<String, String> loginCookies = Jsoup.connect("https://profile.callofduty.com/cod/login").execute().cookies();
-
-        return Jsoup.connect("https://profile.callofduty.com/do_login?new_SiteId=cod")
-                .cookies(loginCookies)
-                .data("username", username)
-                .data("password", password)
-                .data("remember_me", "true")
-                .data("_csrf", loginCookies.get("XSRF-TOKEN"))
-                .method(Connection.Method.POST)
-                .userAgent("PostmanRuntime/7.26.8")
-                .execute();
-    }
-    @Bean
-    public Map<String, String> getCookie() throws IOException {
-        return authToken().cookies();
-    }
 }
--- src/main/java/com/nwerl/warzone/springboot/config/HttpHeadersConfig.java
+++ src/main/java/com/nwerl/warzone/springboot/config/HttpHeadersConfig.java
@@ -1,25 +1,48 @@
 package com.nwerl.warzone.springboot.config;
 
-import lombok.RequiredArgsConstructor;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Configuration
 public class HttpHeadersConfig {
-    private final CookieConfig cookieConfig;
+    @Value("${activision.username}")
+    private String username;
+    @Value("${activision.password}")
+    private String password;
+
+    private Connection.Response authToken() throws IOException {
+        Map<String, String> loginCookies = Jsoup.connect("https://profile.callofduty.com/cod/login").execute().cookies();
+
+        return Jsoup.connect("https://profile.callofduty.com/do_login?new_SiteId=cod")
+                .cookies(loginCookies)
+                .data("username", username)
+                .data("password", password)
+                .data("remember_me", "true")
+                .data("_csrf", loginCookies.get("XSRF-TOKEN"))
+                .method(Connection.Method.POST)
+                .userAgent("PostmanRuntime/7.26.8")
+                .execute();
+    }
+
+    private Map<String, String> getCookie() throws IOException {
+        return authToken().cookies();
+    }
 
     @Bean
     public HttpHeaders getHttpHeaders() throws IOException {
         HttpHeaders headers = new HttpHeaders();
 
         headers.addAll(HttpHeaders.COOKIE,
-                (List<String>)cookieConfig.getCookie().entrySet()
+                (List<String>)getCookie().entrySet()
                 .stream()
                 .map(e -> new String(e.getKey()+"="+e.getValue()))
                 .collect(Collectors.toList()));
